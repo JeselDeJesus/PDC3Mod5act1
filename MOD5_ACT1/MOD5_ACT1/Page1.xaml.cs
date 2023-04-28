3e0a59d287e36105d28ae0438796a12ed07a2bd8
JeselDeJesus <dejesus.jeselaurvic@auf.edu.ph>
2023-04-28 09:16:42 +0800

    final commit

---
+++ MOD5_ACT1/MOD5_ACT1/Page1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MOD5_ACT1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        public void startDate_DateSelected(object sender, EventArgs e)
+        {
+            TimeSpan timeSpan = endDate.Date - startDate.Date;
+            label.Text = $"Total day/s: {timeSpan.Days}";
+        }
+    }
+}
